828cfc3ae7a1af7f49983173abf03137f3b60822
Matthew Bennett <matthew@quickwebdesign.net>
2013-03-19 10:38:44 +0000

    Allow two modes: Restalk.init(:resque), traditional, and Restalk.new(:resque) - Object oriented

--- lib/restalk.rb
+++ lib/restalk.rb
@@ -1,48 +1,56 @@
 
 class Restalk
-  VERSION = '0.0.0.14'
-  def self.init(adapter)
+  VERSION = '0.0.0.15'
+  def initialize(adapter, queue=nil)
     extend BeanstalkAdapter if adapter == :beanstalk
     extend ResqueAdapter if [:resque, :redis].include? adapter
-    super()
+    init queue
   end
 
-  def self.connected?
+  def connected?
     self.respond_to? :push
   end
-  
+
+  def self.init(*args)
+    @current_adapter = self.new *args
+  end
+
+  def self.method_missing(sym, *args, &block)
+    @current_adapter.send sym, *args, &block
+  end
+
   module BeanstalkAdapter
     def init
       require 'beanstalk-client'
-      @@beanstalk = Beanstalk::Pool.new([ENV['BEANSTALK'] || '127.0.0.1:11300'])
+      @beanstalk = Beanstalk::Pool.new([ENV['BEANSTALK'] || '127.0.0.1:11300'])
     end
 
     def push(data)
-      @@beanstalk.put(data)
+      @beanstalk.put(data)
     end
 
     def pop
-      @@beanstalk.reserve
+      @beanstalk.reserve
     end
 
     def stats
-      @@beanstalk.stats
+      @beanstalk.stats
     end
   end
 
   module ResqueAdapter
-    QUEUE = ENV['RESQUE_QUEUE'] || 'restalk_queue'
-    def init
+    def init(queue = nil)
+      @queue = queue || ENV['RESQUE_QUEUE'] || 'restalk_queue'
       require 'resque'
       Resque.redis = ENV['REDIS'] || 'localhost:6379'
     end
 
     def push(data)
-      Resque.enqueue_to(QUEUE, RestalkResqueJob, data)
+      Resque.enqueue_to(@queue, RestalkResqueJob, data)
     end
 
     def pop
-      data = Resque.pop QUEUE
+      data = Resque.pop @queue
       return RestalkResqueJob.new data['args'].first if data
     end
 
